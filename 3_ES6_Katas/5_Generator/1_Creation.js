8a6c639380551aded5fac92b48340695ae3f7eff
hovhrachya93 <hovhrachya93@gmail.com>
2020-04-03 20:11:28 +0400

    jsKatas

---
+++ 3_ES6_Katas/5_Generator/1_Creation.js
@@ -0,0 +1,44 @@
+// 49: Generator - creation
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('Generators can be created in multiple ways', function() {
+  it('the most common way is by adding `*` after `function`', function() {
+    //// function g() {}
+    function* g() {}
+    assertIsGenerator(g());
+  });
+  it('as a function expression, by adding a `*` after `function`', function() {
+    //// let g = function() {};
+    let g = function*() {};
+    assertIsGenerator(g());
+  });
+  it('inside an object by prefixing the function name with `*`', function() {
+    let obj = {
+      //// g() {}
+      *g() {}
+    };
+    assertIsGenerator(obj.g());
+  });
+  it('computed generator names, are just prefixed with a `*`', function() {
+    const generatorName = 'g';
+    let obj = {
+      //// [generatorName]() {}
+      *[generatorName]() {}
+    };
+    assertIsGenerator(obj.g());
+  });
+  it('inside a class the same way', function() {
+    const generatorName = 'g';
+    class Klazz {
+      //// [generatorName]() {}
+      *[generatorName]() {}
+    }
+    assertIsGenerator(new Klazz().g());
+  });
+
+  function assertIsGenerator(gen) {
+    const toStringed = '' + gen;
+    assert.equal(toStringed, '[object Generator]');
+  }
+});
